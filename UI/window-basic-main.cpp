0524ec2af51a9065bd6a3ab79ca325628d5d3325
cg2121 <claytong1214@gmail.com>
2017-06-17 19:10:42 -0500

    UI, libobs: Add ability to lock individual scene items
    Adds a lock checkbox to allow the user to lock a specific scene item.
    
    Closes jp9000/obs-studio#949

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -5005,6 +5005,9 @@ void OBSBasic::Nudge(int dist, MoveDir dir)
 
 	auto func = [] (obs_scene_t*, obs_sceneitem_t *item, void *param)
 	{
+		if (obs_sceneitem_locked(item))
+			return true;
+
 		MoveInfo *info = reinterpret_cast<MoveInfo*>(param);
 		struct vec2 dir;
 		struct vec2 pos;
