6497cd6fd0038e60c028ac85fb49f34611ad8e84
Alex Anderson <anderson.john.alexander@gmail.com>
2018-02-13 20:46:13 -0800

    Update before-deploy-win-rematrix-filter.cmd
    make .zip folder "obs-studio" for easy extraction

--- CI/before-deploy-win-rematrix-filter.cmd
+++ CI/before-deploy-win-rematrix-filter.cmd
@@ -9,4 +9,4 @@ mkdir "C:\projects\rematrix-filter\build\data\obs-plugins\rematrix-filter\locale
 robocopy "C:\projects\obs-studio\build32\rundir\RelWithDebInfo\obs-plugins\32bit" "C:\projects\rematrix-filter\build\obs-plugins\32bit" "rematrix-filter.*"
 robocopy "C:\projects\obs-studio\build64\rundir\RelWithDebInfo\obs-plugins\64bit" "C:\projects\rematrix-filter\build\obs-plugins\64bit" "rematrix-filter.*"
 robocopy "C:\projects\rematrix-filter\data\locale" "C:\projects\rematrix-filter\build\data\obs-plugins\rematrix-filter\locale" "*.*"
-7z a C:\projects\rematrix-filter\build.zip C:\projects\rematrix-filter\build*
+7z a C:\projects\rematrix-filter\obs-studio.zip C:\projects\rematrix-filter\build*
