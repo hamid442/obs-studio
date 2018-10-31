bf9c997e9fc4dd38fb70c8949532c35cd2a4fe7d
Matthew Orlando <cogwheel@caffeine.tv>
2018-10-31 12:00:20 -0700

    UI: Support transient properties

--- UI/properties-view.hpp
+++ UI/properties-view.hpp
@@ -102,6 +102,7 @@ private:
 	void AddFont(obs_property_t *prop, QFormLayout *layout, QLabel *&label);
 	void AddFrameRate(obs_property_t *prop, bool &warning,
 			QFormLayout *layout, QLabel *&label);
+	void SetTransient(obs_property_t *prop);
 
 	void AddProperty(obs_property_t *property, QFormLayout *layout);
 
